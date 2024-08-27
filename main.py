19ba378badd9305e09bcb0ec557535fb018bc443
Enots21 <68638093+Enots21@users.noreply.github.com>
2024-08-27 11:49:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#  1st program
+#Задача 1
+print (9**0.5*5)
+
+# 2nd program
+print(9.99 > 9.98 or 1000 != 1000.1)
+
+# 3rd program
+print(2*2+2, (2*2)+2)
+print(6==6)
+
+# 4th program
+a = float('123.456')
+print(a*10, int(a*10)%10)
+
+#Александров Дании Сергеевич
